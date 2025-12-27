986342e89bcf17d68b31c8f6d8f62f2715196f9a
Richard Ciglanský <ciglansky.richard@gmail.com>
2025-12-27 18:03:19 +0100

    Outputtin tree from SQL database.

---
+++ ImageNetViewer/ImageNetTreeNode.cs
@@ -0,0 +1,8 @@
+﻿namespace ImageNetViewer;
+
+public class ImageNetTreeNode
+{
+    public string Name { get; set; }
+    public int Size { get; set; }
+    public ImageNetTreeNode[] Children { get; set; }
+}
